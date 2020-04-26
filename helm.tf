63351603b112f6ed5883d72d8a88c6a137e5e739
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-04-26 23:22:21 +0100

    first commit

--- helm.tf
+++ helm.tf
@@ -1,10 +1,10 @@
 provider "helm" {
   kubernetes {
     load_config_file       = "false"
-    host                   = "${azurerm_kubernetes_cluster.main.kube_config.0.host}"
-    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)}"
+    host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
   }
 }
 
